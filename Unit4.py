e2b79dfb8599ec0a0ad9770b3ceace9a2b146257
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2019-10-27 09:41:44 -0400

    Update Unit4.py

--- Unit4.py
+++ Unit4.py
@@ -32,3 +32,32 @@ def getWordScore(word, n):
 """
 -----------------------------------------------------------------------------------------------
 """
+"""
+Problem 2
+"""
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+    
+    hand_new = hand.copy()
+    for w in word:
+        hand_new[w] -= 1
+    return hand_new
+"""
+-----------------------------------------------------------------------------------------------
+"""
+
