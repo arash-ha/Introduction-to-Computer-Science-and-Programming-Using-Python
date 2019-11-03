a4560350c6c2895c83b9f7ceb81c33eed9862371
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2019-11-03 07:32:09 -0500

    Update Unit4.py

--- Unit4.py
+++ Unit4.py
@@ -161,3 +161,42 @@ def playHand(hand, wordList, n):
         print('Goodbye! Total score:', ' ', total_score, ' ', 'points.')
     else:
         print('Run out of letters. Total score:', total_score, 'points.')
+
+       
+    
+"""
+-----------------------------------------------------------------------------------------------
+Problem 6
+"""
+
+def playGame(wordList):
+    """
+    Allow the user to play an arbitrary number of hands.
+ 
+    1) Asks the user to input 'n' or 'r' or 'e'.
+      * If the user inputs 'n', let the user play a new (random) hand.
+      * If the user inputs 'r', let the user play the last hand again.
+      * If the user inputs 'e', exit the game.
+      * If the user inputs anything else, tell them their input was invalid.
+ 
+    2) When done playing the hand, repeat from step 1
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+
+    n = HAND_SIZE
+    hand = {}
+
+    while True:  
+        cmd = input('Enter n to deal a new hand, r to replay the last hand, or e to end game:')
+        if cmd == 'r':
+            if not hand:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(hand, wordList, n)
+        elif cmd == 'n':
+            hand = dealHand(n)
+            playHand(hand, wordList, n)
+        elif cmd == 'e':
+            break
+        else:
+            print ("Invalid command.")
