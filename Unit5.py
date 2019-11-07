a484fdc19a799ae9fc0a48ceed610a9b177dfa3c
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2019-11-07 08:45:47 -0500

    Update Unit5.py

--- Unit5.py
+++ Unit5.py
@@ -222,3 +222,53 @@ class PlaintextMessage(Message):
         self.encrypting_dict = super(PlaintextMessage, self).build_shift_dict(shift)
         self.message_text_encrypted = super(PlaintextMessage, self).apply_shift(shift)
 
+"""
+-----------------------------------------------------------------------------------------------
+Problem 3
+"""
+
+class CiphertextMessage(Message):
+    def __init__(self, text):
+        '''
+        Initializes a CiphertextMessage object
+                
+        text (string): the message's text
+
+        a CiphertextMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        #pass #delete this line and replace with your code here
+        Message.__init__(self, text)
+        
+    def decrypt_message(self):
+        '''
+        Decrypt self.message_text by trying every possible shift value
+        and find the "best" one. We will define "best" as the shift that
+        creates the maximum number of real words when we use apply_shift(shift)
+        on the message text. If s is the original shift value used to encrypt
+        the message, then we would expect 26 - s to be the best shift value 
+        for decrypting it.
+
+        Note: if multiple shifts are  equally good such that they all create 
+        the maximum number of you may choose any of those shifts (and their
+        corresponding decrypted messages) to return
+
+        Returns: a tuple of the best shift value used to decrypt the message
+        and the decrypted message text using that shift value
+        '''
+        #pass #delete this line and replace with your code here
+        best_shift = None
+        best_real_words = 0
+        best_msg = ""
+
+        for s in range(26):
+            decrypted_text = self.apply_shift(s)
+            words = decrypted_text.split()
+            real_words = sum([is_word(self.get_valid_words(), w) for w in words])
+            if real_words > best_real_words:
+                best_shift = s
+                best_real_words = real_words
+                best_msg = decrypted_text
+
+        return (best_shift, best_msg)
